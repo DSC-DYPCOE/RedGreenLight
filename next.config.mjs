88a2f19170daeaf04b1dd33cae210016d18bbe61
DJ-BoT07 <djmangaonkar7@gmail.com>
2025-01-18 14:43:12 +0530

    Changed hte mextmj

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Other configurations...
-    async headers() {
-      return [
-        {
-          source: "/api/:path*",
-          headers: [
-            {
-              key: "Access-Control-Allow-Origin",
-              value: "*"  // Or your specific domain
-            },
-            {
-              key: "Access-Control-Allow-Methods",
-              value: "GET, POST, PUT, DELETE, OPTIONS"
-            },
-            {
-              key: "Access-Control-Allow-Headers",
-              value: "Content-Type"
-            }
-          ]
-        }
-      ];
-    },
-  };
-  
+  env: {
+    DB_URL: process.env.DB_URL,
+    NEXT_PUBLIC_SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*"
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS"
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type"
+          }
+        ]
+      }
+    ];
+  }
+};
+
 export default nextConfig;
